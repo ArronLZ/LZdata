f5834db8bf4c56f98c2be112f0d2382d6acbf681
ArronLZ <jiangjianjun@smu.edu.cn>
2024-04-02 00:11:17 +0800

    cre

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' Datasets
+#' LUAD_RESLIST3 contains gene annot info
+#'
+#' @name DataSet
+#' @aliases LUAD_RESLIST3
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Datasets
+#' LUSC_RESLIST3 contains gene annot info
+#'
+#' @name DataSet
+#' @aliases LUSC_RESLIST3
+#' @docType data
+#' @keywords datasets
+NULL
+
+#' Datasets
+#' ESCA_RESLIST3 contains gene annot info
+#'
+#' @name DataSet
+#' @aliases ESCA_RESLIST3
+#' @docType data
+#' @keywords datasets
+NULL
